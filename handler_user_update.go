db5dd257302a97a32cb12a384527fb49c9904e97
tayuelo <tayuelo26@gmail.com>
2025-02-12 23:11:02 -0500

    Authorization: Update User

---
+++ handler_user_update.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"chirpy/internal/auth"
+	"chirpy/internal/database"
+	"encoding/json"
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerUserUpdate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, err.Error(), err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, err.Error(), err)
+		return
+	}
+
+	payload := parameters{}
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&payload)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error(), err)
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error(), err)
+		return
+	}
+
+	user, err := cfg.queries.UpdateUser(r.Context(), database.UpdateUserParams{
+		ID:             userID,
+		Email:          payload.Email,
+		HashedPassword: hashedPassword,
+	})
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error(), err)
+		return
+	}
+
+	type response struct {
+		User
+	}
+
+	respondWithJSON(w, http.StatusOK, response{
+		User: User{
+			ID:        user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email:     user.Email,
+		},
+	})
+}
--- main.go
+++ main.go
@@ -54,6 +54,7 @@ func main() {
 	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUserUpdate)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
